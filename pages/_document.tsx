bca113c7a3387b365582626fa1e0303af368439b
Muhammad Faris <muhfaris@disroot.org>
2023-02-01 00:01:05 +0700

    feat(seo): add metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Trace URL Tool - Muh Faris</title>
+        <meta
+          name="description"
+          content="trace url tool of the suspicious URL until you can see the real URL."
+        />
+        <meta property="og:title" content="Trace URL Tool - Muh Faris" />
+        <meta
+          property="og:description"
+          content="trace url tool of the suspicious URL until you can see the real URL."
+        />
+        <meta property="og:url" content="https://checkurl.muhfaris.com/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
