a40717d69edb11936416c5ae15eeba23ed5b14c6
Muhammad Faris <muhfaris@disroot.org>
2023-02-01 09:23:24 +0700

    fix: change title in app

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Trace URL Tool - Muh Faris</title>
         <script
           defer
           data-domain="checkurl.muhfaris.com"
