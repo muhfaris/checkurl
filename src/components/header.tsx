3b05aafd04aa54eaf6a65c50644f15638cf95159
Muhammad Faris <muhfaris@disroot.org>
2023-02-05 23:41:48 +0700

    feat: add button nihbuatjajan

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
               Check URL
             </span>
           </Link>
-          <div className="flex items-center lg:order-2">
+         <div className="flex items-center lg:order-2">
             <Link
               href="/status-domains"
               className="hidden md:block text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
